da4993923874bb9536f2803beeaa603338fed91b
viatransporte01 <viatransporte01@gmail.com>
2025-04-29 14:30:28 +0000

    Segundo exemplo

--- main.js
+++ main.js
@@ -8,3 +8,15 @@ let somar = function(valor1, valor2) {
 
 console.log(mensagem())
 console.log(somar(10, 20))
+
+let mensagem= () => {
+  console.log("Ola, Tudo bem?")
+} 
+
+let somar = (valor1, valor2) => valor1 + valor2
+
+let raizQuadrada = (valor1) => Math.sqrt(valor1)
+
+console.log(mensagem())
+console.log(somar(25, 768))
+console.log(raizQuadrada(49))
