c9b226dcea0d147fe729449c2fa91e1ade5560d8
viatransporte01 <viatransporte01@gmail.com>
2025-04-29 14:24:52 +0000

    Primeiro exemplo

---
+++ main.js
@@ -0,0 +1,10 @@
+let mensagem = function() {
+  console.log("bem-vindo(a)!")
+}
+
+let somar = function(valor1, valor2) {
+  return valor1 + valor2
+}
+
+console.log(mensagem())
+console.log(somar(10, 20))
